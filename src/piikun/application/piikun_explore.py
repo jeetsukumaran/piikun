3af7d18b840f5958bd9a453874b82af6015b9606
Jeet Sukumaran <jeetsukumaran@gmail.com>
2023-10-05 22:26:44 -0700

    Update

--- src/piikun/application/piikun_explore.py
+++ src/piikun/application/piikun_explore.py
@@ -51,13 +51,14 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 
-def visualize_distances_on_regionalized_support_space_plotly(
+
+def visualize_distances_on_regionalized_support_space(
     df,
     support_quantiles=None,
     distance_quantiles=None,
     gradient_calibration="shared",
-    background_palette="Viridis",
-    scatterplot_palette="Viridis",
+    background_palette="Portland",
+    scatterplot_palette="Portland",
     is_log_scale=True,
 ):
     if not support_quantiles:
@@ -91,7 +92,8 @@ def visualize_distances_on_regionalized_support_space_plotly(
     for i, ptn1_condition in enumerate(range_fns):
         for j, ptn2_condition in enumerate(range_fns):
             subset = bgdf[
-                ptn1_condition(bgdf["ptn1_support"]) & ptn2_condition(bgdf["ptn2_support"])
+                ptn1_condition(bgdf["ptn1_support"])
+                & ptn2_condition(bgdf["ptn2_support"])
             ]
             # mean_values[n_ranges - 1 - i, j] = subset["vi_distance"].mean()
             mean_values[i, j] = subset["vi_distance"].mean()
@@ -116,23 +118,22 @@ def visualize_distances_on_regionalized_support_space_plotly(
     #     for col in df.columns():
     #         content.
 
-    df['scatter_hovertext'] = df.apply(
-        lambda row: f'ptn1: {row.ptn1}<br>ptn2: {row.ptn2}<br>ptn1_support: {row.ptn1_support}<br>ptn2_support: {row.ptn2_support}<br>vi_distance: {row.vi_distance}',
-        axis=1
+    df["scatter_hovertext"] = df.apply(
+        lambda row: f"ptn1: {row.ptn1}<br>ptn2: {row.ptn2}<br>ptn1_support: {row.ptn1_support}<br>ptn2_support: {row.ptn2_support}<br>vi_distance: {row.vi_distance}",
+        axis=1,
     )
 
+    # hover_data = list(df.columns)
     fig.add_trace(
         go.Scatter(
             x=df["ptn1_support"],
             y=df["ptn2_support"],
             mode="markers",
             marker=dict(
-                color=df["vi_distance"],
-                colorscale=scatterplot_palette,
-                size=6
+                color=df["vi_distance"], colorscale=scatterplot_palette, size=6
             ),
-            text=df['scatter_hovertext'],
-            hoverinfo='text'
+            text=df["scatter_hovertext"],
+            hoverinfo="text",
         )
     )
 
@@ -140,10 +141,9 @@ def visualize_distances_on_regionalized_support_space_plotly(
     fig.update_layout(
         xaxis_title="log(ptn1_support)",
         yaxis_title="log(ptn2_support)",
-        title="Visualize Distances on Regionalized Support Space"
+        title="Distances on Regionalized Support Space",
     )
 
-    fig.show()
     return fig
 
 
@@ -174,17 +174,72 @@ def visualize_scatter(
 
     fig = px.scatter(
         df,
-        x='ptn1_support',
-        y='ptn2_support',
-        color='vi_distance',
-        labels={"ptn1_support": "log(ptn1_support)", "ptn2_support": "log(ptn2_support)"},
-        hover_data=['vi_distance'],
-        title="Scatterplot with Hover Annotations"
+        x="ptn1_support",
+        y="ptn2_support",
+        color="vi_distance",
+        labels={
+            "ptn1_support": "log(ptn1_support)",
+            "ptn2_support": "log(ptn2_support)",
+        },
+        hover_data=["vi_distance"],
+        title="Scatterplot with Hover Annotations",
     )
 
-    fig.update_traces(marker=dict(size=5), selector=dict(mode='markers'))
+    fig.update_traces(marker=dict(size=5), selector=dict(mode="markers"))
 
     return fig
+
+
+class PlotGenerator:
+
+    def __init__(
+        self,
+        is_show_plot=False,
+        is_save_plot=True,
+        output_directory=None,
+        output_name_stem=None,
+        output_formats=None,
+    ):
+        self.is_show_plot = is_show_plot
+        self.is_save_plot = is_save_plot
+        if not output_directory:
+            output_directory = os.curdir
+        self.output_directory = pathlib.Path(output_directory)
+        if output_name_stem:
+            self.output_name_stem = output_name_stem
+        else:
+            self.output_name_stem = "piikun"
+        self.output_name_stem = output_name_stem
+        if output_formats is None:
+            self.output_formats = ["html", "jpg"]
+        else:
+            self.output_formats = output_formats
+
+
+    def generate_plot(
+        self,
+        plot_name,
+        plot_fn,
+        plot_kwargs=None,
+        post_plot_fn=None,
+    ):
+        if plot_kwargs is None:
+            plot_kwargs = {}
+        fig = plot_fn(**plot_kwargs)
+        if post_plot_fn:
+            post_plot_fn(fig)
+        for format_type in self.output_formats:
+            if format_type.startswith("."):
+                ext = format_type
+            else:
+                ext = f".{format_type}"
+            output_filepath = self.output_directory / f"{self.output_name_stem}_{plot_name}{ext}"
+            if format_type == "html":
+                fig.write_html(output_filepath)
+            else:
+                fig.write_image(output_filepath)
+        if self.is_show_plot:
+            fig.show()
 
 
 def main(args=None):
@@ -197,35 +252,69 @@ def main(args=None):
         nargs="+",
         help="Path to data source file.",
     )
+    plot_options = parent_parser.add_argument_group("Plot Options")
+    plot_options.add_argument(
+        "--palette",
+        action="store",
+        default="Geyser",
+        help=("Palette or color scheme." " [default='%(default)s']."),
+    )
+    output_options = parent_parser.add_argument_group("Output Options")
+    output_options.add_argument(
+        "-o",
+        "--output-title",
+        action="store",
+        default=None,
+        help="Custom prefix for output filenames (defaults to input filepath stem).",
+    )
+    output_options.add_argument(
+        "-O",
+        "--output-directory",
+        action="store",
+        default=os.curdir,
+        help="Directory for output files [default='%(default)s'].",
+    )
+    output_options.add_argument(
+        "-F",
+        "--output-format",
+        action="append",
+        default=None,
+        help="Output format (multiple formats may be specified; defaults to 'html' and 'jpg').",
+    )
+    output_options.add_argument(
+        "--show-plot",
+        action=argparse.BooleanOptionalAction,
+        dest="is_show_plot",
+        default=False,
+        help="Show / do not show plots in interactive viewer",
+    )
+    output_options.add_argument(
+        "--save-plot",
+        action=argparse.BooleanOptionalAction,
+        dest="is_save_plot",
+        default=True,
+        help="Save / do not save plots in interactive viewer",
+    )
     args = parent_parser.parse_args(args)
     df = utility.read_files_to_dataframe(filepaths=args.src_path)
-    fig = visualize_distances_on_regionalized_support_space_plotly(
-        df=df,
-        # background_palette="Plasma",
-        # scatterplot_palette="Plasma",
-        # background_palette="Turbo",
-        # scatterplot_palette="Turbo",
-        # background_palette="Electric",
-        # scatterplot_palette="electric",
-         # background_palette="Portland",
-        # scatterplot_palette="Portland",
-         # background_palette="Tealrose",
-        # scatterplot_palette="Tealrose",
-         background_palette="Geyser",
-        scatterplot_palette="Geyser",
-         # background_palette="Fall",
-        # scatterplot_palette="Fall",
-         # background_palette="balance",
-        # scatterplot_palette="balance",
-         # background_palette="delta",
-        # scatterplot_palette="delta",
-        )
-    visualization_name = "distance-vs-support-quantiles"
-    format = "html"
-    output_filepath = f"{pathlib.Path(args.src_path[0]).stem}_{visualization_name}.{format}"
-    fig.write_html(output_filepath)
+    plotter = PlotGenerator(
+        is_show_plot = args.is_show_plot,
+        is_save_plot = args.is_save_plot,
+        output_directory = args.output_directory,
+        output_name_stem = args.src_path[0],
+        output_formats = args.output_format,
+    )
+
+    plotter.generate_plot(
+        plot_name = "distance-vs-support-quantiles",
+        plot_fn = visualize_distances_on_regionalized_support_space,
+        plot_kwargs = {
+            "df": df,
+            "background_palette": args.palette,
+            "scatterplot_palette": args.palette,
+        }
+    )
+
 
 if __name__ == "__main__":
     main()
-
-
