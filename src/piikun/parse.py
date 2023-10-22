f94e141aa9eec19769d60e9c1508b37a2c30381d
Jeet Sukumaran <jeetsukumaran@gmail.com>
2023-10-21 19:39:18 -0700

    Update

--- src/piikun/parse.py
+++ src/piikun/parse.py
@@ -107,12 +107,140 @@ def _format_error(format_type, message):
     sys.exit(1)
     # runtime.terminate_error(f"Invalid format: {message}")
 
+# def parse_bpp_a10(
+#     source_stream,
+#     partition_factory,
+# ):
+#     source_data = source_stream.read()
+#     pp_tree_str = parsebpp.extract_bpp_output_posterior_guide_tree_string(source_data)
+#     guide_tree = dendropy.Tree.get_from_string(pp_tree_str, schema="newick", rooting="force-rooted")
+#     clade_roots = []
+#     for nd in guide_tree:
+#         if nd.label:
+#             nd.posterior_probability = float(nd.label[1:])
+#             clade_roots.append(nd)
+#     clade_roots = sorted(clade_roots, key=lambda x: x.posterior_probability, reverse=True)
+#     gt2, result_tree = parsebpp.calculate_bpp_full_species_tree(
+#         src_tree_string=pp_tree_str,
+#         guide_tree=guide_tree,
+#         population_probability_threshold=0.01,
+#     )
+#     terminal_population_clades, lineage_population_clade_map = parsebpp.find_terminal_population_clades(gt2)
+#     for k, v in terminal_population_clades.items():
+#         print(f"--- {k.taxon}")
+#         print(v)
+
 def parse_bpp_a10(
     source_stream,
     partition_factory,
 ):
-    source_data = source_stream.read()
-    guide_tree_str = parsebpp.extract_bpp_output_posterior_guide_tree_string(source_data)
+    import re
+    parsebpp.patterns["a10-species-delimitation-models"] = re.compile(r"^Number of species-delimitation models = (\d+).*$")
+    parsebpp.patterns["a10-species-delimitation-model-row"] = re.compile(r"^ ?(\d+) +([01]+) +([0-9Ee\-.])+ +([0-9Ee\-.])")
+    parsebpp.patterns["a10-species-delimitation-model-header"] = re.compile(r"^ *model +prior +posterior.*$")
+    parsebpp.patterns["a10-species-delimitation-model-row"] = re.compile(r"^ *(\d+?) +([01]+) +([0-9Ee\-.]+) +([0-9Ee\-.]+).*$")
+
+    n_expected_lineages = None
+    n_expected_species_delimitation_models = None
+    n_expected_ancestral_nodes = None
+    lineage_labels = []
+    species_delimitation_model_defs = []
+    ancestral_nodes_labels = []
+
+    current_section = "pre"
+    is_done_processing_rows = None
+    for line_offset, line in enumerate(source_stream):
+        line_idx = line_offset + 1
+        line_text = line.strip()
+        if current_section == "pre":
+            assert current_section == "pre", current_section
+            if line_text.startswith("COMPRESSED ALIGNMENTS") and not line_text.startswith("COMPRESSED ALIGNMENTS AFTER"):
+                current_section = "alignments"
+        elif current_section == "alignments":
+            if lineage_labels and n_expected_lineages and len(lineage_labels) == n_expected_lineages:
+                current_section = "post-alignments"
+                continue
+            if line_text.startswith("COMPRESSED ALIGNMENTS"):
+                continue
+            if not line_text:
+                continue
+            m = parsebpp.patterns["alignment_ntax_nchar"].match(line_text)
+            if m:
+                if n_expected_lineages:
+                    _format_error(format_type="bpp-a10", message=f"Unexpected alignment label and character description (already set to {n_expected_lineages} on {n_expected_lineages_set_on_line_text}): line {line_idx}: '{line_text}'")
+                n_expected_lineages = int(m[1])
+                n_expected_lineages_set_on_line_text = line_idx
+                continue
+            if not n_expected_lineages:
+                # runtime.RuntimeClient._logger.warn(f"Expected alignment label and character description: line {line_idx}: '{line_text}'")
+                _format_error(format_type="bpp-a10", message=f"Missing alignment label and character description: line {line_idx}: '{line_text}'")
+            m = parsebpp.patterns["alignment_sequence"].match(line_text)
+            if not m:
+                _format_error(format_type="bpp-a10", message=f"Expected sequence data: line {line_idx}: '{line_text}'")
+            if len(lineage_labels) == n_expected_lineages:
+                _format_error(format_type="bpp-a10", message=f"Unexpected sequence definition ({n_expected_lineages} labels already parsed): line {line_idx}: '{line_text}'")
+            if m:
+                lineage_labels.append(m[2])
+                continue
+            else:
+                _format_error(format_type="bpp-a10", message=f"Unable to parse sequence: line {line_idx}: '{line_text}'")
+        elif current_section == "post-alignments":
+            m = parsebpp.patterns["a10-species-delimitation-models"].match(line_text)
+            if not m:
+                continue
+            if m:
+                current_section = "species-delimitation-models"
+                n_expected_species_delimitation_models = int(m[1])
+                continue
+        elif current_section == "species-delimitation-models":
+            if not line_text:
+                if is_done_processing_rows is None:
+                    is_done_processing_rows = False
+                    continue
+                elif is_done_processing_rows is False:
+                    # ends at the first blank row
+                    is_done_processing_rows = True
+                    current_section = "post-species-limitation-models"
+                    continue
+            m = parsebpp.patterns["a10-species-delimitation-model-row"].match(line_text)
+            if not m:
+                if not parsebpp.patterns["a10-species-delimitation-model-header"].match(line_text):
+                    _format_error(format_type="bpp-a10", message=f"Expecting model header row: line {line_idx}: '{line_text}'")
+                continue
+            species_delimitation_model_defs.append({
+                "model_id": m[1],
+                "model_code": m[2],
+                "prior": str(m[3]),
+                "posterior": str(m[4]),
+            })
+            continue
+        elif current_section == "post-species-limitation-models":
+            assert len(species_delimitation_model_defs) == n_expected_species_delimitation_models
+            if line_text.startswith("Order of ancestral nodes"):
+                current_section = "ancestral-node-definitions"
+                continue
+        elif current_section == "ancestral-node-definitions":
+            if not line_text:
+                current_section = "post-ancestral-node-definitions"
+                continue
+            n_expected_ancestral_nodes = len(species_delimitation_model_defs[0]["model_code"])
+            ancestral_node_label = line_text.strip()
+            species_subsets = parsebpp.parse_species_subsets(
+                species_subsets_str=ancestral_node_label,
+                lineage_labels=lineage_labels,
+            )
+            print(">>>")
+            print(ancestral_node_label)
+            print("---")
+            print(species_subsets)
+            print("<<<")
+            ancestral_nodes_labels.append(ancestral_node_label)
+    assert len(lineage_labels) == n_expected_lineages
+    assert len(species_delimitation_model_defs) == n_expected_species_delimitation_models, f"{len(species_delimitation_model_defs)} != {n_expected_species_delimitation_models}"
+    assert len(ancestral_nodes_labels) == n_expected_ancestral_nodes
+    # print(lineage_labels)
+    # print(species_delimitation_model_defs)
+
 
 def parse_bpp_a11(
     source_stream,
@@ -123,8 +251,8 @@ def parse_bpp_a11(
     partition_info = []
     line = None
     line_idx = 0
-    n_lineages_expected = None
-    n_lineages_expected_set_on_line = None
+    n_expected_lineages = None
+    n_expected_lineages_set_on_line = None
     n_partitions_expected = None
     for line_offset, line in enumerate(source_stream):
         line_idx = line_offset + 1
@@ -147,27 +275,27 @@ def parse_bpp_a11(
         if current_section == "pre":
             pass
         elif current_section == "alignments":
-            if lineage_labels and n_lineages_expected and len(lineage_labels) == n_lineages_expected:
+            if lineage_labels and n_expected_lineages and len(lineage_labels) == n_expected_lineages:
                 continue
             if line.startswith("COMPRESSED ALIGNMENTS"):
                 continue
             m = parsebpp.patterns["alignment_ntax_nchar"].match(line)
             if m:
-                if n_lineages_expected:
-                    _format_error(format_type="bpp-a11", message=f"Unexpected alignment label and character description (already set to {n_lineages_expected} on {n_lineages_expected_set_on_line}): line {line_idx}: '{line}'")
-                n_lineages_expected = int(m[1])
-                n_lineages_expected_set_on_line = line_idx
+                if n_expected_lineages:
+                    _format_error(format_type="bpp-a11", message=f"Unexpected alignment label and character description (already set to {n_expected_lineages} on {n_expected_lineages_set_on_line}): line {line_idx}: '{line}'")
+                n_expected_lineages = int(m[1])
+                n_expected_lineages_set_on_line = line_idx
                 continue
-            if not n_lineages_expected:
+            if not n_expected_lineages:
                 # runtime.RuntimeClient._logger.warn(f"Expected alignment label and character description: line {line_idx}: '{line}'")
                 _format_error(format_type="bpp-a11", message=f"Missing alignment label and character description: line {line_idx}: '{line}'")
             m = parsebpp.patterns["alignment_sequence"].match(line)
             if not m:
                 _format_error(format_type="bpp-a11", message=f"Expected sequence data: line {line_idx}: '{line}'")
-            if len(lineage_labels) == n_lineages_expected:
-                _format_error(format_type="bpp-a11", message=f"Unexpected sequence definition ({n_lineages_expected} labels already parsed): line {line_idx}: '{line}'")
+            if len(lineage_labels) == n_expected_lineages:
+                _format_error(format_type="bpp-a11", message=f"Unexpected sequence definition ({n_expected_lineages} labels already parsed): line {line_idx}: '{line}'")
             if m:
-                lineage_labels.append(m[1])
+                lineage_labels.append(m[2])
                 continue
             else:
                 _format_error(format_type="bpp-a11", message=f"Unable to parse sequence: line {line_idx}: '{line}'")
@@ -184,8 +312,8 @@ def parse_bpp_a11(
             #         lineage_labels = [label for label in parsebpp.patterns["strip_tree_tokens"].split(m[5]) if label]
             #         # print(lineage_labels)
         elif current_section == "(B)":
-            if len(lineage_labels) != n_lineages_expected:
-                _format_error(format_type="bpp-a11", message=f"{n_lineages_expected} lineages expected but {len(lineage_labels)} lineages identified: {lineage_labels}")
+            if len(lineage_labels) != n_expected_lineages:
+                _format_error(format_type="bpp-a11", message=f"{n_expected_lineages} lineages expected but {len(lineage_labels)} lineages identified ({lineage_labels}): line {line_idx}: '{line}'")
             if line.startswith("(B)"):
                 runtime.RuntimeClient._logger.info(f"{len(lineage_labels)} lineages identified: {lineage_labels}")
                 continue
@@ -195,21 +323,24 @@ def parse_bpp_a11(
             frequency = float(parts[1])
             num_subsets = int(parts[2])
             species_subsets_str = " ".join(parts[3:]).strip("()")
-            species_subsets = []
-            current_subset = []
-            temp_lineage_label = ""
-            for char in species_subsets_str:
-                if char == ' ':
-                    if current_subset:
-                        species_subsets.append(current_subset)
-                        current_subset = []
-                else:
-                    temp_lineage_label += char
-                    if temp_lineage_label in lineage_labels:
-                        current_subset.append(temp_lineage_label)
-                        temp_lineage_label = ""
-            if current_subset:
-                species_subsets.append(current_subset)
+            # species_subsets = []
+            # current_subset = []
+            # temp_lineage_label = ""
+            # for char in species_subsets_str:
+            #     if char == ' ':
+            #         if current_subset:
+            #             species_subsets.append(current_subset)
+            #             current_subset = []
+            #     else:
+            #         temp_lineage_label += char
+            #         if temp_lineage_label in lineage_labels:
+            #             current_subset.append(temp_lineage_label)
+            #             temp_lineage_label = ""
+
+            species_subsets = parsebpp.parse_species_subsets(
+                species_subsets_str=species_subsets_str,
+                lineage_labels=lineage_labels,
+            )
             partition_d = {
                 "frequency": frequency,
                 "n_subsets": num_subsets,
--- src/piikun/parsebpp.py
+++ src/piikun/parsebpp.py
@@ -4,8 +4,8 @@
 import re
 
 patterns = {
-    "alignment_ntax_nchar": re.compile("^\s*(\d+)\s*(\d+)\s*$", re.MULTILINE),
-    "alignment_sequence": re.compile("^\^(\S+).*$", re.MULTILINE),
+    "alignment_ntax_nchar": re.compile("^\s*(\d+)\s*(\d+)\s*.*$", re.MULTILINE),
+    "alignment_sequence": re.compile("^(.*?\^)?(\S+).*$", re.MULTILINE),
     "a11_section_b": re.compile(r"\(B\)\s*(\d+) species delimitations .*$", re.MULTILINE),
     "spd_pattern_end": re.compile(r"\(C\)\s*", re.MULTILINE),
     "a11_treemodel_entry": re.compile(r"^(\d+) ([0-9.]+) ([0-9.]+) (\d+) (.*;).*$"),
@@ -16,6 +16,27 @@ patterns = {
 # a11_treemodel_entry = re.compile(r"^\s*(\d+)\s+([0-9.]+)\s+([0-9.]+)\s+(\d+)\s+(.*;)\s+(.*)$")
 
 
+def parse_species_subsets(
+    species_subsets_str,
+    lineage_labels,
+):
+    species_subsets = []
+    current_subset = []
+    temp_lineage_label = ""
+    for char in species_subsets_str:
+        if char == ' ':
+            if current_subset:
+                species_subsets.append(current_subset)
+                current_subset = []
+        else:
+            temp_lineage_label += char
+            if temp_lineage_label in lineage_labels:
+                current_subset.append(temp_lineage_label)
+                temp_lineage_label = ""
+    if current_subset:
+        species_subsets.append(current_subset)
+    return species_subsets
+
 def check_for_overlapping_labels(labels):
     for i, label1 in enumerate(labels):
         for j, label2 in enumerate(labels):
@@ -23,52 +44,52 @@ def check_for_overlapping_labels(labels):
                 return True, (label1, label2)
     return False, None
 
-def parse_species_delimitation_data(labels, src_data):
-    labels = sorted(labels, key=len, reverse=True)
-    in_relevant_section = False
-    parsed_data = []
-    n_partitions_expected = None
-    for line in src_data.split("\n"):
-        line = line[:-1].strip()
-        if not line:
-            continue
-        m = a11_section_b.match(line)
-        if m:
-            in_relevant_section = True
-            n_partitions_expected = int(m[1])
-            continue
-        if in_relevant_section and spd_pattern_end.match(line):
-            break
-        # Parse lines within the relevant section.
-        if in_relevant_section:
-            assert n_partitions_expected
-            parts = line.strip().split()
-            frequency = float(parts[1])
-            num_subsets = int(parts[2])
-            species_subsets_str = " ".join(parts[3:]).strip("()")
-            species_subsets = []
-            current_subset = []
-            temp_label = ""
-            for char in species_subsets_str:
-                if char == ' ':
-                    if current_subset:
-                        species_subsets.append(current_subset)
-                        current_subset = []
-                else:
-                    temp_label += char
-                    if temp_label in labels:
-                        current_subset.append(temp_label)
-                        temp_label = ""
-            if current_subset:
-                species_subsets.append(current_subset)
-            assert len(species_subsets) == num_subsets
-            parsed_data.append({
-                "frequency": frequency,
-                "n_subsets": num_subsets,
-                "subsets": species_subsets
-            })
-    assert len(parsed_data) == n_partitions_expected
-    return parsed_data
+# def parse_species_delimitation_data(labels, src_data):
+#     labels = sorted(labels, key=len, reverse=True)
+#     in_relevant_section = False
+#     parsed_data = []
+#     n_partitions_expected = None
+#     for line in src_data.split("\n"):
+#         line = line[:-1].strip()
+#         if not line:
+#             continue
+#         m = a11_section_b.match(line)
+#         if m:
+#             in_relevant_section = True
+#             n_partitions_expected = int(m[1])
+#             continue
+#         if in_relevant_section and spd_pattern_end.match(line):
+#             break
+#         # Parse lines within the relevant section.
+#         if in_relevant_section:
+#             assert n_partitions_expected
+#             parts = line.strip().split()
+#             frequency = float(parts[1])
+#             num_subsets = int(parts[2])
+#             species_subsets_str = " ".join(parts[3:]).strip("()")
+#             species_subsets = []
+#             current_subset = []
+#             temp_label = ""
+#             for char in species_subsets_str:
+#                 if char == ' ':
+#                     if current_subset:
+#                         species_subsets.append(current_subset)
+#                         current_subset = []
+#                 else:
+#                     temp_label += char
+#                     if temp_label in labels:
+#                         current_subset.append(temp_label)
+#                         temp_label = ""
+#             if current_subset:
+#                 species_subsets.append(current_subset)
+#             assert len(species_subsets) == num_subsets
+#             parsed_data.append({
+#                 "frequency": frequency,
+#                 "n_subsets": num_subsets,
+#                 "subsets": species_subsets
+#             })
+#     assert len(parsed_data) == n_partitions_expected
+#     return parsed_data
 
 # test_contents = [
 #     "(VARIABLE)",
@@ -130,19 +151,168 @@ def parse_bpp_ctl(control_filepath):
 # ):
 #     with open(bpp_output_filepath) as src:
 #         return parse_bpp_a11(src)
+#   return species_info, partition_info
 
-    return species_info, partition_info
+# def find_terminal_population_clades(tree):
+#     """
+#     Takes a tree with nodes *annotated" to indicate whether or not they should
+#     be collapsed and identifies the set of 'terminal population clades'.
 
-def extract_bpp_output_posterior_guide_tree_string(source_text):
-    # pretty dumb logic for now: just locates the last non-blank line
-    # works with: bp&p Version 3.1, April 2015, in "10" mode, i.e. infer species delimitation with guide tree.
-    lines = source_text.split("\n")
-    result = None
-    for idx, line in enumerate(lines[-1::-1]):
-        if line:
-            result = line
-            break
-    return result
+#     'Terminal population clades' are substrees descending from nodes with no
+#     ancestors in a collapsed state AND either: (a) no child nodes [i.e., a
+#     leaf] or (b) no descendent nodes in a non-collapsed or open states.
+#     Nodes corresponding to terminal populations themselves are, by definition,
+#     need to in a collapsed state unless they are leaves.
+
+#     Returns a dictionary mapping terminal population nodes to leaf nodes descending
+#     from the terminal population nodes.
+#     """
+#     # ensure if parent is collapsed, all children are collapsed
+#     for gnd in tree.preorder_node_iter():
+#         if gnd.parent_node is not None and gnd.parent_node.annotations["is_collapsed"].value:
+#             gnd.annotations["is_collapsed"] = True
+#         else:
+#             gnd.annotations["is_collapsed"] = False
+#     # identify the lowest nodes (closest to the tips) that are open, and
+#     # add its children if the children are (a) leaves; or (b) themselves
+#     # are closed
+#     terminal_population_clades = {}
+#     lineage_population_clade_map = {}
+#     for nd in tree.postorder_node_iter():
+#         if nd.annotations["is_collapsed"].value:
+#             continue
+#         for child in nd.child_node_iter():
+#             if child.is_leaf():
+#                 terminal_population_clades[child] = set([child])
+#                 lineage_population_clade_map[child] = child
+#             elif child.annotations["is_collapsed"].value:
+#                 terminal_population_clades[child] = set([desc for desc in child.leaf_iter()])
+#                 for lnd in terminal_population_clades[child]:
+#                     lineage_population_clade_map[lnd] = child
+#     for nd in tree:
+#         if nd not in terminal_population_clades:
+#             nd.annotations["population_id"] = "0"
+#             continue
+#         if nd.is_leaf():
+#             nd.annotations["population_id"] = nd.taxon.label
+#         else:
+#             nd.annotations["population_id"] = "+".join(desc.taxon.label for desc in nd.leaf_iter())
+#         # print("{}: {}".format(nd.annotations["population"], len(terminal_population_clades[nd])))
+#     return terminal_population_clades, lineage_population_clade_map
+
+# def calculate_bpp_full_species_tree(
+#         src_tree_string,
+#         guide_tree,
+#         population_probability_threshold=0.95):
+#     import dendropy
+#     from dendropy.calculate import treecompare
+#     # Logic:
+#     # - Any internal node label is assumed to be a bpp annotation in the
+#     #   form of "#<float>" indicating the posterior probability of the node.
+#     # - If not found, assigned posterior probability of 0 if internal node or 1 if leaf.
+#     # - In post-order, collapse all nodes with less than threshold pp
+#     # - Revisit tree, assign taxon labels
+#     tree0 = dendropy.Tree.get(
+#             data=src_tree_string,
+#             schema="newick",
+#             rooting="force-rooted",
+#             suppress_external_node_taxa=False,
+#             suppress_internal_node_taxa=True,
+#             taxon_namespace=guide_tree.taxon_namespace,
+#             )
+#     tree0.encode_bipartitions()
+#     guide_tree.encode_bipartitions()
+#     try:
+#         diff = treecompare.symmetric_difference(tree0, guide_tree)
+#         assert diff == 0
+#     except AssertionError:
+#         print("[BPP tree]{}".format(tree0.as_string("newick")))
+#         print("[Guide tree]{}".format(guide_tree.as_string("newick")))
+#         print(f"RF = {diff}")
+#         raise
+#     for nd in tree0:
+#         edge_len = guide_tree.bipartition_edge_map[nd.edge.bipartition].length
+#         if edge_len is not None and nd.edge.length is not None:
+#             nd.edge.length = edge_len
+#         if nd.is_leaf():
+#             nd.pp = 1.0
+#             nd.label = nd.taxon.label
+#         elif nd.label:
+#             nd.pp = float(nd.label[1:])
+#             nd.label = None
+#         else:
+#             nd.pp = 0.0
+
+#     tree1 = dendropy.Tree(tree0)
+#     tree1.taxon_namespace = dendropy.TaxonNamespace()
+
+#     nodes_to_process = [tree1.seed_node]
+#     while nodes_to_process:
+#         nd = nodes_to_process.pop(0)
+#         if nd.is_leaf():
+#             pass
+#         elif nd.pp < population_probability_threshold:
+#             desc_tips = []
+#             for sub_nd in nd.leaf_iter():
+#                 desc_tips.append(sub_nd)
+#             # nd.set_child_nodes(new_children)
+#             len_to_add = 0.0
+#             subnode = desc_tips[0]
+#             while subnode is not nd:
+#                 if subnode.edge.length is not None:
+#                     len_to_add += subnode.edge.length
+#                 subnode = subnode.parent_node
+#             child_labels = [c.label for c in desc_tips]
+#             nd.label = "+".join(child_labels)
+#             if nd.edge.length is not None and len_to_add is not None:
+#                 nd.edge.length += len_to_add
+#             nd.child_labels = child_labels
+#             nd.clear_child_nodes()
+#             guide_tree_edge = guide_tree.bipartition_edge_map[nd.edge.bipartition]
+#             guide_tree_edge.head_node.is_collapsed = True
+#         else:
+#             for sub_nd in nd.child_node_iter():
+#                 nodes_to_process.append(sub_nd)
+#     collapsed_nodes = set()
+#     for gnd in guide_tree.postorder_node_iter():
+#         if getattr(gnd, "is_collapsed", False):
+#             len_to_add = 0.0
+#             desc_nd = gnd
+#             while True:
+#                 try:
+#                     desc_nd = desc_nd._child_nodes[0]
+#                     if desc_nd.edge.length is not None and len_to_add is not None:
+#                         len_to_add += desc_nd.edge.length
+#                 except IndexError:
+#                     break
+#             if gnd.edge.length is not None and len_to_add is not None:
+#                 gnd.edge.length += len_to_add
+#             for xnd in gnd.postorder_iter():
+#                 if xnd is gnd:
+#                     continue
+#                 xnd.edge.length = 1e-10
+#             gnd.annotations["is_collapsed"] = True
+#         else:
+#             gnd.annotations["is_collapsed"] = False
+#     for gnd in guide_tree.preorder_node_iter():
+#         if gnd.parent_node is not None and gnd.parent_node.annotations["is_collapsed"].value:
+#             gnd.annotations["is_collapsed"] = True
+#     for nd in tree1.leaf_node_iter():
+#         nd.taxon = tree1.taxon_namespace.require_taxon(label=nd.label)
+#         nd.label = None
+
+#     return guide_tree, tree1
+
+# def extract_bpp_output_posterior_guide_tree_string(source_text):
+#     # pretty dumb logic for now: just locates the last non-blank line
+#     # works with: bp&p Version 3.1, April 2015, in "10" mode, i.e. infer species delimitation with guide tree.
+#     lines = source_text.split("\n")
+#     result = None
+#     for idx, line in enumerate(lines[-1::-1]):
+#         if line:
+#             result = line
+#             break
+#     return result
 
 
 # read_bpp_a11(
